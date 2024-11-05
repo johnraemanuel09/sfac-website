efc46501a98ebfd83b4070534e69b8f86b08656a
John Rae G. Manuel <159091163+johnraemanuel09@users.noreply.github.com>
2024-11-05 19:52:17 +0800

    Hero Section

---
+++ src/pages/components/Hero2.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import basicEd from '../../../public/basicEd.jpg';
+import higherEd from '../../../public/higherEd.jpg';
+
+const Hero2 = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -mx-4 -mb-10 text-center">
+            <div className="sm:w-1/2 mb-10 px-4">
+              <div className="rounded-lg h-64 overflow-hidden">
+              <img alt="content" className="object-contain object-center h-full w-full" src="/basicEd.jpg" width={1201} height={502} />
+              </div>
+              <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">
+                BASIC EDUCATION DEPARTMENT
+              </h2>
+              <p className="leading-relaxed text-base">
+                Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.
+              </p>
+              <button className="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">
+                Button
+              </button>
+            </div>
+            <div className="sm:w-1/2 mb-10 px-4">
+              <div className="rounded-lg h-64 overflow-hidden">
+              <img alt="content" className="object-contain object-center h-full w-full" src="/higherEd.jpg" width={1201} height={502} />
+              </div>
+              <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">
+                HIGHER EDUCATION DEPARTMENT
+              </h2>
+              <p className="leading-relaxed text-base">
+                Williamsburg occupy sustainable snackwave gochujang. Pinterest cornhole brunch, slow-carb neutra irony.
+              </p>
+              <button className="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">
+                Button
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Hero2;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import localFont from "next/font/local";
-
+import Hero2 from "./components/Hero2";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      
+      <Hero2 />
       {/* Other content */}
     </div>
   );
