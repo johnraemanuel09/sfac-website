e6b1957964bebc1bc65bb1c7987ffbc2c81e31bd
John Rae G. Manuel <garciajohnmanuel016@gmail.com>
2024-11-13 14:59:52 +0800

    Update Hero2.jsx

--- src/pages/components/Hero2.jsx
+++ src/pages/components/Hero2.jsx
@@ -9,29 +9,31 @@ const Hero2 = () => {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -mx-4 -mb-10 text-center">
             <div className="sm:w-1/2 mb-10 px-4">
-              <div className="rounded-lg h-64 overflow-hidden">
-              <img alt="content" className="object-contain object-center h-full w-full" src="/basicEd.jpg" width={1201} height={502} />
+              <div className="rounded-lg h-80 overflow-hidden">
+                <img
+                  alt="content"
+                  className="object-contain object-center h-full w-full"
+                  src="/basicEd.jpg"
+                />
               </div>
               <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">
                 BASIC EDUCATION DEPARTMENT
               </h2>
-              <p className="leading-relaxed text-base">
-                
-              </p>
               <button className="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">
                 Button
               </button>
             </div>
             <div className="sm:w-1/2 mb-10 px-4">
-              <div className="rounded-lg h-64 overflow-hidden">
-              <img alt="content" className="object-contain object-center h-full w-full" src="/higherEd.jpg" width={1201} height={502} />
+              <div className="rounded-lg h-80 overflow-hidden">
+                <img
+                  alt="content"
+                  className="object-contain object-center h-full w-full"
+                  src="/higherEd.jpg"
+                />
               </div>
               <h2 className="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">
                 HIGHER EDUCATION DEPARTMENT
               </h2>
-              <p className="leading-relaxed text-base">
-                
-              </p>
               <button className="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-5 focus:outline-none hover:bg-indigo-600 rounded">
                 Button
               </button>
